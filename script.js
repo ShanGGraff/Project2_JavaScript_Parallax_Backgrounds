8269409a37a1f5609525a94eaa30135868c818a7
Shane Graff <copellius@gmail.com>
2022-09-26 12:35:00 -0700

    Add background layers

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 5;
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = "assets/layer-1.png";
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = "assets/layer-2.png";
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = "assets/layer-3.png";
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = "assets/layer-4.png";
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = "assets/layer-5.png";
+
+let x = 0;
+
+function animate () {
+  ctx.drawImage(backgroundLayer4, 0, 0)
+  requestAnimationFrame(animate);
+};
+animate();
